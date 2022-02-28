25e18fe014d13123a604ffb126f812404dadcd21
VaneS2 <74139708+VaneS2@users.noreply.github.com>
2022-02-28 22:13:24 +0100

    q
    q

--- demo5PPPPPPPPPPPPPPPP/demo5PPPPPPPPPPPPPPPP/src/main/java/com/example/demo/repository/CoronaEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.CoronaEvents;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoronaEventRepository extends JpaRepository<CoronaEvents,Long> {
-
-
-}
